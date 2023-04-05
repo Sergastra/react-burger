a806b37013519670206296c520761cc7fe490343
Sergey <sergastrah@yandex.ru>
2023-04-05 22:40:52 +0400

    Добавил пустые папки

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
 import './app.css';
 import AppHeader from '../AppHeader/app-header'
 
 function App() {
   return (
-    <>
+    <div>
       <AppHeader />
-    </>
+      <main>
+
+      </main>
+    </div>
   );
 }
 export default App;
