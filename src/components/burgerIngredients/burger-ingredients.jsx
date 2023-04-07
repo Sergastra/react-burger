a784d6901bcebe89c14031d8b8689d212546b4be
Sergey <sergastrah@yandex.ru>
2023-04-07 17:50:19 +0400

    добавил useRef

--- src/components/burgerIngredients/burger-ingredients.jsx
+++ src/components/burgerIngredients/burger-ingredients.jsx
@@ -1,18 +1,40 @@
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
-import { useState } from "react";
+import { useState, useRef } from "react";
+
 const BurgerIngredients = ({ ingredients }) => {
-    const [current, setCurrent] = useState('bun')
+    const [current, setCurrent] = useState('bun');
+
+    const typeIngredient = ["bun", "sauce", "main"];
+    const scollTobunRef = useRef();
+    const scollTosauceRef = useRef();
+    const scollTomainRef = useRef();
+
+    const translate =
+    {
+        bun: 'Булки',
+        sauce: 'Соусы',
+        main: 'Начинки'
+    }
   return (
-    <div style={{ display: 'flex' }}>
-      <Tab value="bun" active={current === 'Булки'} onClick={setCurrent}>
-        One
-      </Tab>
-      <Tab value="souce" active={current === 'Соусы'} onClick={setCurrent}>
-        Two
-      </Tab>
-      <Tab value="main" active={current === 'Начинки'} onClick={setCurrent}>
-        Three
-      </Tab>
+    <div style={{ display: "flex" }}>
+          <Tab value="bun" active={current === "bun"} onClick={(e) => {
+              scollTobunRef.current.scrollIntoView({ behavior: "smoot" })
+              setCurrent(e)
+          }}>
+              Булки
+          </Tab>
+          <Tab value="sauce" active={current === "sauce"} onClick={(e) => {
+              scollTosauceRef.current.scrollIntoView({ behavior: "smoot" })
+              setCurrent(e)
+          }}>
+              Соусы
+          </Tab>
+          <Tab value="main" active={current === "main"} onClick={(e) => {
+              scollTomainRef.current.scrollIntoView({ behavior: "smoot" })
+              setCurrent(e)
+          }}>
+              Начинки
+          </Tab>
     </div>
   )
 }
