5eb856c6144b87b8581b330f03ee8fd3a365f583
Sergey <sergastrah@yandex.ru>
2023-04-07 09:53:45 +0400

    Добавил Preloader

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -4,24 +4,33 @@ import AppHeader from '../AppHeader/app-header';
 import BurgerConstructor from '../burgerConstructor/burger-constructor';
 import BurgerIngredients from '../burgerIngredients/burger-ingredients';
 import { getIngredients } from '../utils/app';
+import Preloader from '../preloader/preloader';
 
 
 function App() {
   const [ingredients, setIngredients] = useState([]);
+  const [ingredientsLoading, setIngredientsLoading] = useState(true);
   useEffect(() => {
    getIngredients()
-   .then(setIngredients);
+   .then(setIngredients)
+   .catch(() => alert("Во время загрузки произошла ошибка"))
+   .finally(() => setIngredientsLoading(false))
   }, []);
   
 
   return (
     <div>
       <AppHeader />
+      {ingredientsLoading ? (
+        <Preloader/>
+      ) : (
       <main>
         <BurgerConstructor ingredients={ingredients}/>
         <BurgerIngredients ingredients={ingredients}/>
       </main>
+      )}
     </div>
+    
   );
 }
 export default App;
---
+++ src/components/preloader/preloader.jsx
@@ -0,0 +1,9 @@
+const Preloader = () => {
+    return (
+        <div>
+            <h1>Загрузка.....Ждите</h1>
+        </div>
+    );
+}
+
+export default Preloader;
