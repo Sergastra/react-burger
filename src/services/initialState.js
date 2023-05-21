be49b7e2950ef0e7cb11c561bb044be9dfe5eefb
Sergey <sergastrah@yandex.ru>
2023-05-21 10:30:51 +0400

    Добавил хранилище store-redux и подключил

---
+++ src/services/initialState.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    data: [],
+    burgerIngredients: {
+        bun: null,
+        contentItems: [],
+        counts: {}
+    },
+    modal: {},
+    order: {
+        number: 0
+    }
+};
--- src/utils/api.js
+++ src/utils/api.js
@@ -14,7 +14,6 @@ export const getIngredients = () => {
 };
 
 /* универсальная функция */
-
 function request(url, options) {
   return fetch(url, options).then(checkResponse)
 };
