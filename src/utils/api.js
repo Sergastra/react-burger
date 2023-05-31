91c2b8218554f84affd82c605bd1a7ff2ae17eb5
Sergey <sergastrah@yandex.ru>
2023-05-31 18:12:48 +0400

    сделал actions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,19 +1,13 @@
 const BURGER_URL = "https://norma.nomoreparties.space/api";
+export const createOrderUrl = BURGER_URL + '/orders';
 
 const checkResponse = (res) => {
   return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 };
 
-export const getIngredients = () => {
-  return request(`${BURGER_URL}/ingredients`)
-    .then(data => {
-      if (data?.success) return data.data;
-      return Promise.reject(data)
-    }
-  );
-};
+
 
 /* универсальная функция */
-function request(url, options) {
+export function request(url, options) {
   return fetch(url, options).then(checkResponse)
 };
